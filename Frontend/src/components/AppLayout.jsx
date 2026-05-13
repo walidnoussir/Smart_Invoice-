b38ec1244d15c3ec25340f1e77bc8bb646bea383
walidnoussir <noussirw@gmail.com>
2026-05-13 09:35:00 +0100

    modify something

--- Frontend/src/components/AppLayout.jsx
+++ Frontend/src/components/AppLayout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 
 import Dashboard from "./Dashboard";
-import NavBar from "./NavBar";
 
 function AppLayout() {
   return (
     <div>
-      <NavBar />
       <Dashboard />
       <main>
         <Outlet />
