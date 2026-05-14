8dcfca1247a3a5998a8b0e4d093cfcac969ce665
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-05-14 10:03:06 +0100

    l'ajout de la route vers dashboard page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import AppLayout from "./components/AppLayout";
 import HomPage from "./pages/HomPage";
 import SupplierPage from "./pages/SupplierPage";
 import InvoicePage from "./pages/InvoicePage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<HomPage />} />
           <Route path="suppliers" element={<SupplierPage />} />
           <Route path="invoices" element={<InvoicePage />} />
+          <Route path="dashboard" element={<DashboardPage/>} />
         </Route>
       </Routes>
     </BrowserRouter>
