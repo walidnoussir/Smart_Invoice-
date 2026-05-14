6d96016f197db36486a95d6f1b2ac8e96cacf591
Asma Adani <dianiasmaa27328@gmail.com>
2026-05-14 10:24:21 +0100

    gérer les erreurs et lancer server

--- Frontend/src/components/AppLayout.jsx
+++ Frontend/src/components/AppLayout.jsx
@@ -15,15 +15,15 @@ function AppLayout() {
   return (
     <div className="flex gap-2.5 h-screen">
 
-    <button
+    {/* <button
       onClick={handleLogout}
       className="bg-red-500 text-white px-4 py-2 rounded-lg"
     >
       Logout
-    </button>
+    </button> */}
 
       {/* <NavBar /> */}
-      <Dashboard />
+      <Dashboard  />
       <main className="w-full overflow-scroll">
         <Outlet />
       </main>
